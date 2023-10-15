9e67d133b707c199d8dbdacbf47a0f84588b68c0
Michel de Bree <michel@micheldebree.nl>
2023-10-15 08:45:08 +0200

    img2petscii: bumped version

--- img2petscii/src/cli.ts
+++ img2petscii/src/cli.ts
@@ -9,7 +9,7 @@ import { writeFile } from 'node:fs/promises'
 import { Config, saveConfig, loadConfig, CharsetType, fromCliOptions, CliOptions } from './config.js'
 
 // TODO get version from package.json
-const version = '0.0.5'
+const version = '0.0.6'
 const cols = 40
 const rows = 25
 const width: number = cols * 8
